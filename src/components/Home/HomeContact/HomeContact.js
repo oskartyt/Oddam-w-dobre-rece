f0eae885cf46390ee8fb6bcc1dcfe9d8c341a3d9
oskartyt <tytusoskar@gmail.com>
2019-09-19 16:38:25 +0200

    Form Validation Done

--- src/components/Home/HomeContact/HomeContact.js
+++ src/components/Home/HomeContact/HomeContact.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './HomeContact.scss';
+import {isJSXNamespacedName} from "@babel/types";
 
 class HomeContact extends Component{
     state={
@@ -11,7 +12,8 @@ class HomeContact extends Component{
         emailError:false,
         messageError:false,
 
-        messageSend:false
+        messageSend:false,
+        fetchError:false
 
     };
     handleChange = e =>{
@@ -29,42 +31,90 @@ class HomeContact extends Component{
                 name, email, message
             })
         })
-                .then(function (data) {
+                .then((data)=> {
                     console.log(data);
+                    this.setState({messageSend:true,fetchError:false})
                 })
-                .catch(function (error) {
+                .catch((error)=> {
                     console.log(error);
+                    this.setState({messageSend:false,fetchError:true});
                 })
     };
 
     handleSubmit=(e)=>{
         e.preventDefault();
+        this.setState({messageSend:false});
         let name=this.state.name;
         let email=this.state.email;
         let message=this.state.message;
+        let correctMessage=true;
 
-        this.sendForm(name,email,message);
+        let nameRegex=/^[a-zA-Z]{1,200}/;
+
+        let emailRegex= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        let messageRegex=/^.{120,1000}/;
+
+        console.log('Regex mail',emailRegex.test(email));
+        console.log('Reg Name',nameRegex.test(name));
+        console.log("reg mess", messageRegex.test(message));
+
+        if (!nameRegex.test(name)){
+            this.setState({nameError:true});
+            correctMessage=false;
+        }else{
+            this.setState({nameError:false});
+        }
+
+        if (!emailRegex.test(email)){
+            this.setState({emailError:true});
+            correctMessage=false;
+        }else{
+            this.setState({emailError:false});
+        }
+
+        if (!messageRegex.test(message)){
+            this.setState({messageError:true});
+            correctMessage=false;
+        }else{
+            this.setState({messageError:false});
+        }
+
+        if (correctMessage){
+            this.sendForm(name,email,message);
+        }
 
 
-    }
+
+    };
     render() {
         return(
             <div className='contact' name="homeContact">
                 <form onSubmit={this.handleSubmit}>
                     <h2>Skontaktuj się z nami</h2>
                     <div className='decoration'/>
+                    <div className='messageSend'>
+                        {this.state.messageSend && <p className='send'>Wiadomość została wysłana!<br/>Wkrótce się skonatktujemy.</p>}
+                        {this.state.fetchError && <p className='error'>Bład serwera!</p>}
+                        {(!this.state.messageSend && !this.state.fetchError) && <p/>}
+
+
+                        </div>
                     <div className='nameAndEmail'>
                         <div className='singleField'>
                             <label htmlFor='contactName'>Wpisz swoje imię</label>
                             <input type="text"  placeholder='Imię' value={this.state.name} name='name' onChange={this.handleChange}/>
+                            {(this.state.nameError)?<span>Podane imię jest nieprawidłowe!</span>:<span className='correct'/>}
                         </div>
                         <div className='singleField'>
                             <label htmlFor='contactMail'>Wpisz swój email</label>
                             <input type="text" placeholder='e-mail@xyz.com' value={this.state.email} name='email' onChange={this.handleChange}/>
+                            {(this.state.emailError)?<span>Podane email jest nieprawidłowy!</span>:<span className='correct'/>}
                         </div>
                     </div>
                     <label htmlFor='contactMessage'>Wpisz swoja wiadomość</label>
                     <textarea rows="4" placeholder='Lorem Ipsum Fixum Dyrdum' value={this.state.message} name='message' onChange={this.handleChange}/>
+                    {(this.state.messageError)?<span>Widomość musi miec  conajmniej 120 znaków!</span>:<span className='correct'/>}
                     <div className='submit'>
                         <input type='submit' value="Wyślij"/>
                     </div>
