9d269b538e6f30ddafe24f484235550ea9570403
oskartyt <tytusoskar@gmail.com>
2019-09-20 14:58:06 +0200

    Login validation done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,6 +8,36 @@ import {Link} from 'react-router-dom';
 
 
 class Login extends Component{
+    state={
+        email:'',
+        password:'',
+        emailError:false,
+        passwordError:false,
+    };
+    handleChange = e =>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    };
+    handleSubmit=e=>{
+        e.preventDefault();
+        let email=this.state.email;
+        let password=this.state.password;
+
+        let emailRegex= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (!emailRegex.test(email)){
+            this.setState({emailError:true});
+        }else{
+            this.setState({emailError:false});
+        }
+
+        if (password.length<6){
+            this.setState({passwordError:true});
+        }else{
+            this.setState({passwordError:false});
+        }
+    };
     render() {
         return(
             <>
@@ -18,17 +48,17 @@ class Login extends Component{
                 <div className='login'>
                     <h2>Zaloguj się</h2>
                     <div className='decoration'/>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div className='login-form-fields'>
                             <div className='single-field'>
                                 <label htmlFor="">Email</label>
-                                <input type="text"/>
-                                <span className='correct'/>
+                                <input type="text" value={this.state.email} name='email' onChange={this.handleChange}/>
+                                {(this.state.emailError)?<span>Podany email jest nieprawidłowy!</span>:<span className='correct'/>}
                             </div>
                             <div className='single-field'>
                                 <label htmlFor="">Hasło</label>
-                                <input type="text"/>
-                                <span className='correct'/>
+                                <input type="password" value={this.state.password} name='password' onChange={this.handleChange}/>
+                                {(this.state.passwordError)?<span>Podane hasło jest za krótkie!</span>:<span className='correct'/>}
                             </div>
                         </div>
                         <div className='login-form-buttons'>
